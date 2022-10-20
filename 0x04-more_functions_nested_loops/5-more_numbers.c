d3dedbd78e58abecb85bf930e8a422bc9a6fc01d
Igbaji Joseph <zeusbenson3@gmail.com>
2022-10-20 07:53:28 -0700

    morenumbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints digits 10x 
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	char c, j;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar('1');
+				_puchar((j%10) + '0');
+			}
+		}
+		_puchar('i\n');
+	}
+}
