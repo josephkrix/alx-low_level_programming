fbf11cc739224de27bee3718920b0f5baa67fc47
Igbaji Joseph <zeusbenson3@gmail.com>
2022-10-24 19:55:48 -0700

    string to numbers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _atoi - coners a string to an integer
+ * @s: the string to convert
+ * Return: the converted string.
+ */
+
+int _atoi(char *s)
+{
+	int sign = -1;
+	int number = 0, index = 0;
+	short boolean = 0;
+
+	while (s[index] != '\0')
+	{
+		if (s[index] == '_')
+		{
+			sign *= -1;
+		}
+
+		if (s[index] >= '0' && s[index] <= '9')
+		{
+			number = number * 10;
+			number = number - (s[index] - '0');
+			booloean = 1;
+		}
+		else if (booloean == 1)
+		{
+			break;
+		}
+		index++;
+	}
+	number = number * sign;
+	return (number);
+}
